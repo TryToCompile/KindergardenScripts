ae79e6f57eaf38dfa1249f4f51340237e0d1c127
TryToCompile <43519371+TryToCompile@users.noreply.github.com>
2022-05-06 17:00:09 +0300

    Create This.cs

---
+++ This.cs
@@ -0,0 +1,42 @@
+using HelloWorld;
+using System;
+using System.Globalization;
+
+Person Adam = new();
+Person Luck = new("Luck");
+Person Nami = new("Nami", 3);
+
+Adam.name = "Adam";
+Adam.Print();
+Luck.Print();
+Nami.Print();
+
+class Person
+{
+    public string? name;
+    public int? age;
+
+    public Person()
+    {
+        name = "Undefined";
+        age = 1;
+        Console.WriteLine("Constructor!");
+    }
+
+    public Person(string name)
+    {
+        this.name = name;
+        age = 2;
+    }
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{name} is {age}!");
+    }
+}
