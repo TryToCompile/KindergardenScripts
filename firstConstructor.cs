62a9cf39a11ebcde2c20b6d265519aa7869d1352
TryToCompile <43519371+TryToCompile@users.noreply.github.com>
2022-05-06 16:46:19 +0300

    Update: added undefined lana

--- firstConstructor.cs
+++ firstConstructor.cs
@@ -2,8 +2,10 @@ using System;
 using System.Globalization;
 
 
-Person Tom = new Person();
+Person Tom = new();
+Person Lana = new("lana", 19);
 
+Lana.Print();
 Tom.Print();
 
 
@@ -13,15 +15,28 @@ class Person
     public int ?age = 0;
     public Person() // Happens only when we declaring an object
     {
-        Console.WriteLine("It's constructor! Input name:");
+        Console.WriteLine("It's 1 constructor! Input name:");
         Name = Console.ReadLine();
         Console.WriteLine("Input age:");
         age = int.Parse(Console.ReadLine());
+    }
+    
+    public Person(string name,int age)
+    {
+        Console.WriteLine("It's 2 constructor!");
     }
 
 
     public void Print()
     {
-        Console.WriteLine($"{Name} is {age}.");
+        if (Name != "Undefined")
+        {
+            Console.WriteLine($"{Name} is {age}.");
+        } else
+        {
+            age = 0;
+            Console.WriteLine($"{Name} person is {age}.");
+        }
+        
     }
 }
