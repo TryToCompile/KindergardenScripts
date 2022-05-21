e6be07376a72c56f82b6de59b77392e0fb32a0ee
TryToCompile <43519371+TryToCompile@users.noreply.github.com>
2022-05-21 07:59:43 +0300

    Solved StringRepeat cata, 7 kyu

---
+++ SolvedCatas/StringRepeat.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+using Codewars;
+
+
+Kata kata = new();
+
+Console.WriteLine(kata.RepeatStr(2,"*"));
+
+namespace Codewars
+{
+    class Kata
+    {
+        public string RepeatStr(int n, string s)
+        {
+            string sum = "";
+            for (; n > 0; n--)
+            {
+                sum += s;
+            }
+            return sum;
+        }
+    }
+}
