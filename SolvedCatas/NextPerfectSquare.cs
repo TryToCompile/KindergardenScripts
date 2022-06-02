ae924fe70202892398406d499ea056162ab9e042
TryToCompile <43519371+TryToCompile@users.noreply.github.com>
2022-06-02 17:17:11 +0300

    Update NextPerfectSquare.cs

--- SolvedCatas/NextPerfectSquare.cs
+++ SolvedCatas/NextPerfectSquare.cs
@@ -8,9 +8,7 @@ Console.WriteLine(FindNextSquare(144));
  long FindNextSquare(long num)
 {
     double result = Math.Sqrt(num);
-    Console.WriteLine(result);
     bool isSquare = result % 1 == 0; //  result - n * result, where n -> largest possible integer that is less than or equal to |x| / |y|
-    Console.WriteLine(isSquare);
     if (isSquare == true)
     {
         result++;
