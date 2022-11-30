822da379e3e635422e58073f1d8457dfa81ef922
TryToCompile <adam1999@tut.by>
2022-11-30 17:24:47 +0300

    Add: factorial, again.

---
+++ FromVsCode/app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+
+var count = new Maths();
+double n = Convert.ToDouble(ReadLine());
+WriteLine(count.Factorial(n));
+
+class Maths
+{
+    public double Factorial(double n)
+    {
+        if (n < 0) return 0;
+        if (n == 0) return n + 1;
+        if (n == 1) return 1;
+        return n * Factorial(n - 1);
+    }
+}
